7d0330f2579fe3bc712b2ca81b6ffa9259f0fef7
Adam Hansen <adamlars90@gmail.com>
2017-01-31 19:06:05 +0100

    Added modal feature for assignment 4

--- src/bikeshop/gui/controller/SingleBikeController.java
+++ src/bikeshop/gui/controller/SingleBikeController.java
@@ -6,11 +6,18 @@
 package bikeshop.gui.controller;
 
 import bikeshop.be.BikeProperty;
+import bikeshop.gui.model.BikeModel;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -26,8 +33,6 @@ public class SingleBikeController implements Initializable {
     @FXML
     private Label lblPrice;
 
-    private BikeProperty mBikeModel;
-
     /**
      * Initializes the controller class.
      */
@@ -37,10 +42,27 @@ public class SingleBikeController implements Initializable {
     }
 
     public void setModel(BikeProperty model) {
-        mBikeModel = model;
-        lblType.textProperty().bind(mBikeModel.typeProperty());
-        lblSeller.textProperty().bind(mBikeModel.sellerProperty());
-        lblPrice.textProperty().bind(mBikeModel.priceProperty().asString());
+        lblType.textProperty().bind(model.typeProperty());
+        lblSeller.textProperty().bind(model.sellerProperty());
+        lblPrice.textProperty().bind(model.priceProperty().asString());
+    }
+
+    @FXML
+    private void handleShowBike() throws IOException {
+
+        Stage primStage = (Stage) lblType.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/bikeshop/gui/view/SingleBike.fxml"));
+        Parent root = loader.load();
+        SingleBikeController controller = loader.getController();
+        //TODO ALH: MAKE DYNAMIC!
+        controller.setModel(BikeModel.getInstance().getObservableBikes().get(0));
+        Stage editStage = new Stage();
+        editStage.setScene(new Scene(root));
+
+        editStage.initModality(Modality.WINDOW_MODAL);
+        editStage.initOwner(primStage);
+
+        editStage.show();
     }
 
 }
